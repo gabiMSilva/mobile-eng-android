cfe8f58cf784cc65215c5f5fee0855e74345ad33
Gabriela Medeiros da Silva <gabriela.medeiros2010@gmail.com>
2024-08-17 12:34:30 -0300

    ListView

--- app/src/main/java/com/gmsilva/basiccomponents/InsetoModel.kt
+++
@@ -1,9 +0,0 @@
-package com.gmsilva.basiccomponents
-
-import android.graphics.drawable.Drawable
-
-data class InsetoModel (
-    val title: String = "",
-    val description: String = "",
-    val image: Drawable? = null
-)
