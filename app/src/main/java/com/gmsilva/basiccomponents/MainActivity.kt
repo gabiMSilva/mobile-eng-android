55a0ceecd0da0a9471516f70c29967768085d1b3
Gabriela Medeiros da Silva <gabriela.medeiros2010@gmail.com>
2024-10-14 15:34:48 -0300

    Usando temas

--- app/src/main/java/com/gmsilva/basiccomponents/MainActivity.kt
+++ app/src/main/java/com/gmsilva/basiccomponents/MainActivity.kt
@@ -1,83 +1,75 @@
 package com.gmsilva.basiccomponents
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.gmsilva.basiccomponents.databinding.ActivityMainBinding
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.IOException
 
-class MainActivity : AppCompatActivity(), OnItemClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var noticias: List<NoticiaModel> = listOf()
-    private lateinit var adapter: ArrayAdapter<NoticiaModel>
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private val THEME_PREFERENCES_LABEL = "THEME_PREFERENCES"
+    private val THEME_MODE_LABEL = "THEME_MODE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        sharedPreferences = getSharedPreferences(THEME_PREFERENCES_LABEL, Context.MODE_PRIVATE)
+
+        recoveryAndApplyTheme();
         setContentView(binding.root)
-
-        setupListView()
+        setupToolbar()
     }
 
-    private fun setupListView() {
-        noticias = readJson(this)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = NoticiaAdapter(
-            noticias, Glide.with(this), this@MainActivity
-        )
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
     }
 
-    private fun readJson(context: Context): List<NoticiaModel>{
-        val listTemp = mutableListOf<NoticiaModel>()
+    private fun applyTheme(theme: Int) {
+        AppCompatDelegate.setDefaultNightMode(theme)
+    }
 
-        try {
+    private fun recoveryAndApplyTheme() {
+        applyTheme(getThemePreference());
+    }
 
-            val json = context.assets.open("data.json").bufferedReader().use { it.readText() }
-            val jsonArray = JSONArray(json)
+    private fun getThemePreference(): Int {
+        return sharedPreferences.getInt(THEME_MODE_LABEL, MODE_NIGHT_NO)
+    }
 
-            for (i in 0 until jsonArray.length()) {
-                val jsonObject = jsonArray.getJSONObject(i)
-                val nomeAutor = jsonObject.getString("nomeAutor")
-                val imagemAutor = jsonObject.getString("imagemAutor")
-                val imagemNoticia = jsonObject.getString("imagemNoticia")
-                val titulo = jsonObject.getString("titulo")
-                val link = jsonObject.getString("link")
-                val data = jsonObject.getString("data")
-                val corpo = jsonObject.getString("corpo")
+    private fun saveThemePreference(theme: Int) {
+        sharedPreferences.edit().putInt(THEME_MODE_LABEL, theme).apply()
+    }
 
-                val noticia = NoticiaModel(
-                    imagemNoticia = imagemNoticia,
-                    data = data,
-                    link = link,
-                    nomeAutor = nomeAutor,
-                    imagemAutor = imagemAutor,
-                    titulo = titulo,
-                    corpo = corpo
-                )
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_theme, menu)
+        return true
+    }
 
-                listTemp.add(noticia)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_theme_light -> {
+                applyTheme(MODE_NIGHT_NO)
+                saveThemePreference(MODE_NIGHT_NO)
+                true
             }
-        } catch (error: Exception ) {
-            println("Erro na leitura do json")
+
+            R.id.menu_theme_dark -> {
+                applyTheme(MODE_NIGHT_YES)
+                saveThemePreference(MODE_NIGHT_YES)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
-        return listTemp
-
-    }
-
-    override fun openLink(link: String) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(link)
-        startActivity(intent)
     }
 }
